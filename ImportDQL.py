6b3b24397f010061d63a9f7c245e401d5e538ada
Arthur Langlard <arthur.electron@orange.fr>
2022-02-24 16:10:42 +0100

    Adding sources.

---
+++ ImportDQL.py
@@ -0,0 +1,98 @@
+# ##### BEGIN GPL LICENCE BLOCK #####
+#  Copyright (C) 2022  Arthur Langlard
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENCE BLOCK #####
+
+
+# Author: Arthur Langlard, arthur.langlard@universite-paris-saclay.fr
+# Start of the project: 24-02-2022
+# Last modification: 24-02-2022
+#
+# This software is a plugin for the Veusz software.
+# it is designed to load DQL files produced by the X-ray diffractometer
+# D-8 Brucker. DQL files are text files containing experimental parameters
+# and data. This plugin loads a DQL file, and imports only the information
+# stored below the [Data] tag.
+# Two datasets are created: one containing the 2-theta angle of diffraction
+# and one containing the corresponding intensity of the diffracted beam.
+
+from veusz.plugins import ImportPlugin, importpluginregistry
+
+class ImportDQL(ImportPlugin):
+    name = "DQL"
+    author = "Arthur Langlard"
+    description = "Imports X-ray diffraction measurements stored in a DQL file."
+
+    # Comment this line to remove the tab of the plugin
+    promote_tab = 'DQL'
+    file_extensions = set(['.dql', '.DQL'])
+
+    def formatLine(self, line):
+        # Removal of all spaces.
+        return line.replace(' ', '')
+    
+    def getNpArray(self, file):
+        import numpy as np
+        dataFlag = False
+
+        data = []
+        
+        for line in file:
+            if line == '[Data]\r\n' or line == '[Data]\n':  # Only interested in the [Data] part.
+                dataFlag = True
+            
+            if dataFlag:
+                data.append(self.formatLine(line).split(",")[:-1])  # Remove the \n character at the end of the line.
+
+        return (data[1], np.array(data[2:]))    # Return (Names, Data).
+
+
+    def __init__(self):
+        from veusz.plugins import ImportPlugin
+        ImportPlugin.__init__(self)
+
+
+    def getPreview(self, params):
+        import numpy as np
+        f = params.openFileWithEncoding()
+
+        npArray = self.getNpArray(f)
+        longString = str(npArray[0][0]) + ',' + str(npArray[0][1]) + '\n'
+        for data in npArray[1]:
+            longString = longString + str(data[0]) + ',' + str(data[0]) + '\n'
+
+        return (longString, True)
+    
+
+    def doImport(self, params):
+        from veusz.plugins import ImportDataset1D
+        import numpy as np
+        """Actually import data
+        params is a ImportPluginParams object.
+        Return a list of ImportDataset1D, ImportDataset2D objects
+        """
+        f = params.openFileWithEncoding()
+
+
+        names, npData = self.getNpArray(f)
+
+        # Return two 1D datasets: one containing the Angle and the other containing the PSD value.
+        # (PSD: Position-sensitive-detector)
+        return [ImportDataset1D(names[0], npData[:,0]),
+        ImportDataset1D(names[1], npData[:,1])]
+
+# add the class to the registry.
+importpluginregistry.append(ImportDQL)
